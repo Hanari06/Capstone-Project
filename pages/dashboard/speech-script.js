85bc3aac4af336368d91ef999ed91d5344eae90d
Hanna Trisha B. Napa <trisha.napa@gmail.com>
2023-08-23 09:13:52 +0800

    WIP Speech Recognition

--- pages/dashboard/speech-script.js
+++ pages/dashboard/speech-script.js
@@ -5,23 +5,8 @@ let okraBtn = document.getElementById("btn-green-choices");
 let ubasBtn = document.getElementById("btn-blue-choices");
 let batoBtn = document.getElementById("btn-purple-choices");
 let listenBtn = document.getElementById("listenBtn");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let wordDisplay = document.getElementById("word-display");
+let currentWord = "aso";
 
 
 
@@ -36,104 +21,133 @@ let SpeechRecognition =
   recognition,
   recording = false;
 
-function populateLanguages() {
-  languages.forEach((lang) => {
-    const option = document.createElement("option");
-    option.value = lang.code;
-    option.innerHTML = lang.name;
-    inputLanguage.appendChild(option);
-  });
-}
+// function populateLanguages() {
+//   languages.forEach((lang) => {
+//     const option = document.createElement("option");
+//     option.value = lang.code;
+//     option.innerHTML = lang.name;
+//     inputLanguage.appendChild(option);
+//   });
+// }
 
-populateLanguages();
 
-function speechToText() {
-  try {
-    recognition = new SpeechRecognition();
-    recognition.lang = inputLanguage.value;
-    recognition.interimResults = true;
-    recordBtn.classList.add("recording");
-    recordBtn.querySelector("p").innerHTML = "Listening...";
-    recognition.start();
-    recognition.onresult = (event) => {
-      const speechResult = event.results[0][0].transcript;
-      //detect when intrim results
-      if (event.results[0].isFinal) {
-        result.innerHTML += " " + speechResult;
-        result.querySelector("p").remove();
-      } else {
-        //creative p with class interim if not already there
-        if (!document.querySelector(".interim")) {
-          const interim = document.createElement("p");
-          interim.classList.add("interim");
-          result.appendChild(interim);
-        }
-        //update the interim p with the speech result
-        document.querySelector(".interim").innerHTML = " " + speechResult;
-      }
-      downloadBtn.disabled = false;
-    };
-    recognition.onspeechend = () => {
-      speechToText();
-    };
-    recognition.onerror = (event) => {
-      stopRecording();
-      if (event.error === "no-speech") {
-        alert("No speech was detected. Stopping...");
-      } else if (event.error === "audio-capture") {
-        alert(
-          "No microphone was found. Ensure that a microphone is installed."
-        );
-      } else if (event.error === "not-allowed") {
-        alert("Permission to use microphone is blocked.");
-      } else if (event.error === "aborted") {
-        alert("Listening Stopped.");
-      } else {
-        alert("Error occurred in recognition: " + event.error);
-      }
-    };
-  } catch (error) {
-    recording = false;
 
-    console.log(error);
-  }
-}
 
-recordBtn.addEventListener("click", () => {
+
+// recordBtn.addEventListener("click", () => {
   
-    result.innerHTML = "";
-    speechToText();
-    recording = true;
-    setTimeout(stopRecording, 1500);
-});
+//     result.innerHTML = "";
+//     recording = true;
+
+//     recognition = new SpeechRecognition();
+//     recognition.lang = inputLanguage.value;
+//     recognition.interimResults = true;
+//     recordBtn.classList.add("recording");
+//     recordBtn.querySelector("p").innerHTML = "Nakikinig...";
+
+//     try {
+    
+//     recognition.start();
+//     recognition.onresult = (event) => {
+//       onCheckResult(event);
+//     };
+//     recognition.onspeechend = () => {
+//       speechToText();
+//     };
+//     recognition.onerror = (event) => {
+//       stopRecording();
+//       if (event.error === "no-speech") {
+//         alert("No speech was detected. Stopping...");
+//       } else if (event.error === "audio-capture") {
+//         alert(
+//           "No microphone was found. Ensure that a microphone is installed."
+//         );
+//       } else if (event.error === "not-allowed") {
+//         alert("Permission to use microphone is blocked.");
+//       } else if (event.error === "aborted") {
+//         alert("Listening Stopped.");
+//       } else {
+//         alert("Error occurred in recognition: " + event.error);
+//       }
+//     };
+    
+//   } catch (error) {
+//     recording = false;
+
+//     console.log(error);
+//   }
+    
+// });
+
+
 
 function stopRecording() {
   recognition.stop();
-  recordBtn.querySelector("p").innerHTML = "Start Listening";
+  recordBtn.querySelector("p").innerHTML = "Makinig";
   recordBtn.classList.remove("recording");
   recording = false;
 }
 
-function download() {
-  const text = result.innerText;
-  const filename = "speech.txt";
 
-  const element = document.createElement("a");
-  element.setAttribute(
-    "href",
-    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
-  );
-  element.setAttribute("download", filename);
-  element.style.display = "none";
-  document.body.appendChild(element);
-  element.click();
-  document.body.removeChild(element);
+const wordList = ["aso", "elepante", "ibon", "okra", "ubas", "bato"];
+const buttonList = [asoBtn, elepanteBtn, ibonBtn, okraBtn, ubasBtn, batoBtn];
+const wordDisplayList = ["a-so", "e-le-pan-te", "i-bon", "ok-ra", "u-bas", "ba-to"];
+let count = 0;
+
+
+recordBtn.addEventListener("click", startRecognition);
+
+
+function startRecognition() {
+  // recordBtn.innerHTML = "Nakinig...";
+
+  result.innerText = ``;
+
+  recognition = new SpeechRecognition();
+  recognition.lang = "tl";
+
+
+  recognition.interimResults = true;
+  recordBtn.classList.add("recording"); 
+  recordBtn.querySelector("p").innerHTML = "Nakikinig...";
+
+  
+  recognition.onresult = (event) => {
+    
+    console.log(event);
+
+    const speechResult = event.results[0][0].transcript.toLowerCase();
+
+
+    if (speechResult.includes(currentWord)) {
+      result.innerHTML = "";
+      let newPrompt = confirm("Ipatuloy?");
+      setTimeout(() => {
+        if (newPrompt == true) {
+          startRecognition();
+        } else {
+          recordBtn.querySelector("p").innerHTML = "Makinig";
+          return;
+        }
+      }, 100)
+      if (count === 6) return;
+      count++;
+      result.innerHTML = currentWord;
+      currentWord = wordList[count];
+      buttonList[count].disabled = false;
+      wordDisplay.innerText = wordDisplayList[count];
+      
+      if (newPrompt == false) return;
+      // stopRecording();
+    }
+
+    if (event.results[0].isFinal) {
+        result.innerHTML += " " + speechResult;
+        // result.querySelector("p").remove();
+    } 
+
+
+  };
+  
+  recognition.start();
 }
-
-downloadBtn.addEventListener("click", download);
-
-clearBtn.addEventListener("click", () => {
-  result.innerHTML = "";
-  downloadBtn.disabled = true;
-});
-
